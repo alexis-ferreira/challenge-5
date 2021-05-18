5516fe8a1813416d4b6a7e4ae06f2286454766e7
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-05-19 01:18:39 +0200

    Update

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let slider = ["./images/image-tanya.jpg", "./images/image-john.jpg"]
+// console.log(slider);
+
+let img = document.getElementById('imgSlider');
+    console.log(img);
+let imgSrc = img.getAttribute('src');
+    console.log(imgSrc);
+
+let btnPreview = document.getElementById('preview-img');
+// console.log(btnPreview);
+let btnNext = document.getElementById('next-img');
+// console.log(btnNext);
+
+
+function changeSlide(){
+    
+    console.log(imgSrc);
+    imgSrc == slider[0] ? img.setAttribute('src',slider[1]) : img.setAttribute('src',slider[0])
+    console.log(imgSrc);
+    
+    
+}
+
+btnPreview.addEventListener('click',changeSlide);
